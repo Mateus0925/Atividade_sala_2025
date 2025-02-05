27e00f4342299a21503ad12bc5e72536ba295450
Mateus Fernandes <josemateus0925@gmail.com>
2025-02-05 12:46:04 -0300

    sixth commit

---
+++ Js/Aprendizado/aula_funcao.js
@@ -0,0 +1,24 @@
+const x = 5;
+
+const y = 8;
+
+function soma(num1,num2){
+    return num1 + num2
+}
+
+console.log(soma(x,y))
+
+// função flexa 
+
+const soma2 = (num1, num2) => {
+    return num1 + num2 
+}
+
+const dobro = (x) => x*2
+console.log(dobro(2))
+
+//Atividade 
+
+const calcularMedia = (num1,num2,num3) => (num1+num2+num3)/3
+
+console.log(calcularMedia(6,6,6))
